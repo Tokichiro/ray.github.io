786a7efec6faa7fba29966780f3607b9bfd10888
Tokichiro <78749319+Tokichiro@users.noreply.github.com>
2021-03-10 22:23:44 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,325 @@
+
+
+
+
+$('#animation').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('#animation').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ }); 
+ }); 
+ 
+ $('#animation1').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('#animation1').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ }); 
+ }); 
+
+ $('#animation2').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('#animation2').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ }); 
+ }); 
+
+ $('#animation3').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('#animation3').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ }); 
+ }); 
+
+
+ $('#animation4').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('#animation4').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ }); 
+ }); 
+ 
+ $('#animation5').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('#animation5').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ }); 
+ }); 
+
+ $('#animation6').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('#animation6').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ }); 
+ }); 
+
+ $('#animation7').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('#animation7').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ }); 
+ }); 
+
+ $('#animation8').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('#animation8').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ }); 
+ }); 
+
+ $('#animation9').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('#animation9').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ }); 
+ }); 
+
+ $('#animation10').css('visibility','hidden');
+ $(window).scroll(function(){
+  var windowHeight = $(window).height(),
+      topWindow = $(window).scrollTop();
+  $('#animation10').each(function(){
+   var targetPosition = $(this).offset().top;
+   if(topWindow > targetPosition - windowHeight + 100){
+    $(this).addClass("fadeInDown");
+   }
+  }); 
+  }); 
+
+  $('#animation11').css('visibility','hidden');
+  $(window).scroll(function(){
+   var windowHeight = $(window).height(),
+       topWindow = $(window).scrollTop();
+   $('#animation11').each(function(){
+    var targetPosition = $(this).offset().top;
+    if(topWindow > targetPosition - windowHeight + 100){
+     $(this).addClass("fadeInDown");
+    }
+   }); 
+   }); 
+
+    $('#animation12').css('visibility','hidden');
+    $(window).scroll(function(){
+    var windowHeight = $(window).height(),
+        topWindow = $(window).scrollTop();
+    $('#animation12').each(function(){
+    var targetPosition = $(this).offset().top;
+    if(topWindow > targetPosition - windowHeight + 100){
+    $(this).addClass("fadeInDown");
+    }
+    }); 
+    }); 
+
+    $('#animation13').css('visibility','hidden');
+    $(window).scroll(function(){
+    var windowHeight = $(window).height(),
+        topWindow = $(window).scrollTop();
+    $('#animation13').each(function(){
+    var targetPosition = $(this).offset().top;
+    if(topWindow > targetPosition - windowHeight + 100){
+    $(this).addClass("fadeInDown");
+    }
+    }); 
+    }); 
+
+    $('#animation14').css('visibility','hidden');
+    $(window).scroll(function(){
+    var windowHeight = $(window).height(),
+        topWindow = $(window).scrollTop();
+    $('#animation14').each(function(){
+    var targetPosition = $(this).offset().top;
+    if(topWindow > targetPosition - windowHeight + 100){
+        $(this).addClass("fadeInDown");
+    }
+    }); 
+    }); 
+
+    $('#animation15').css('visibility','hidden');
+    $(window).scroll(function(){
+    var windowHeight = $(window).height(),
+        topWindow = $(window).scrollTop();
+    $('#animation15').each(function(){
+        var targetPosition = $(this).offset().top;
+        if(topWindow > targetPosition - windowHeight + 100){
+        $(this).addClass("fadeInDown");
+        }
+    }); 
+    }); 
+
+
+    $('#animation16').css('visibility','hidden');
+    $(window).scroll(function(){
+        var windowHeight = $(window).height(),
+            topWindow = $(window).scrollTop();
+        $('#animation16').each(function(){
+        var targetPosition = $(this).offset().top;
+        if(topWindow > targetPosition - windowHeight + 100){
+        $(this).addClass("fadeInDown");
+        }
+        }); 
+        }); 
+
+        
+        $('#animation17').css('visibility','hidden');
+        $(window).scroll(function(){
+        var windowHeight = $(window).height(),
+            topWindow = $(window).scrollTop();
+        $('#animation17').each(function(){
+        var targetPosition = $(this).offset().top;
+        if(topWindow > targetPosition - windowHeight + 100){
+        $(this).addClass("fadeInDown");
+        }
+        }); 
+        }); 
+
+        $('#animation18').css('visibility','hidden');
+        $(window).scroll(function(){
+        var windowHeight = $(window).height(),
+            topWindow = $(window).scrollTop();
+        $('#animation18').each(function(){
+        var targetPosition = $(this).offset().top;
+        if(topWindow > targetPosition - windowHeight + 100){
+            $(this).addClass("fadeInDown");
+        }
+        }); 
+        }); 
+
+        
+        $('#animation19').css('visibility','hidden');
+        $(window).scroll(function(){
+        var windowHeight = $(window).height(),
+            topWindow = $(window).scrollTop();
+        $('#animation19').each(function(){
+            var targetPosition = $(this).offset().top;
+            if(topWindow > targetPosition - windowHeight + 100){
+            $(this).addClass("fadeInDown");
+            }
+        }); 
+        }); 
+
+        $('#animation20').css('visibility','hidden');
+        $(window).scroll(function(){
+        var windowHeight = $(window).height(),
+            topWindow = $(window).scrollTop();
+        $('#animation20').each(function(){
+            var targetPosition = $(this).offset().top;
+            if(topWindow > targetPosition - windowHeight + 100){
+            $(this).addClass("fadeInDown");
+            }
+        }); 
+        }); 
+
+        $('#animation21').css('visibility','hidden');
+        $(window).scroll(function(){
+        var windowHeight = $(window).height(),
+            topWindow = $(window).scrollTop();
+        $('#animation21').each(function(){
+            var targetPosition = $(this).offset().top;
+            if(topWindow > targetPosition - windowHeight + 100){
+            $(this).addClass("fadeInDown");
+            }
+        }); 
+        }); 
+
+        $('#animation22').css('visibility','hidden');
+        $(window).scroll(function(){
+        var windowHeight = $(window).height(),
+            topWindow = $(window).scrollTop();
+        $('#animation22').each(function(){
+            var targetPosition = $(this).offset().top;
+            if(topWindow > targetPosition - windowHeight + 100){
+            $(this).addClass("fadeInDown");
+            }
+        }); 
+        }); 
+
+        $('#animation23').css('visibility','hidden');
+        $(window).scroll(function(){
+        var windowHeight = $(window).height(),
+            topWindow = $(window).scrollTop();
+        $('#animation23').each(function(){
+            var targetPosition = $(this).offset().top;
+            if(topWindow > targetPosition - windowHeight + 100){
+            $(this).addClass("fadeInDown");
+            }
+        }); 
+        }); 
+
+        $('#animation24').css('visibility','hidden');
+        $(window).scroll(function(){
+        var windowHeight = $(window).height(),
+            topWindow = $(window).scrollTop();
+        $('#animation24').each(function(){
+            var targetPosition = $(this).offset().top;
+            if(topWindow > targetPosition - windowHeight + 100){
+            $(this).addClass("fadeInDown");
+            }
+        }); 
+        }); 
+
+        $('#animation25').css('visibility','hidden');
+        $(window).scroll(function(){
+        var windowHeight = $(window).height(),
+            topWindow = $(window).scrollTop();
+        $('#animation25').each(function(){
+            var targetPosition = $(this).offset().top;
+            if(topWindow > targetPosition - windowHeight + 100){
+            $(this).addClass("fadeInDown");
+            }
+        }); 
+        }); 
+
+        
+
+
+
+
